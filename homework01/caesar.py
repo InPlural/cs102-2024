e23781f558e1e0859b3353d79896c625e2e303fd
InPlural <arinadautova21@gmail.com>
2024-10-22 15:35:07 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,6 +1,7 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
+
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -11,13 +12,32 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+        final = ''
+        if char == ' ':
+            ciphertext += ' '
+        elif char in '.,':
+            ciphertext += char
+        elif char.isdigit():
+            ciphertext += char
+        elif char.isalpha():
+            stay = ord(char) + shift
+            if char.islower():
+                if stay > ord('z'):
+                    stay -= 26
+            if char.isupper():
+                if stay > ord('Z'):
+                    stay -= 26
+            final = chr(stay)
+        ciphertext += final
     return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -28,5 +48,23 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+        final = ''
+        if char == ' ':
+            plaintext += ' '
+        elif char in '.,':
+            plaintext += char
+        elif char.isdigit():
+            plaintext += char
+        elif char.isalpha():
+            stay = ord(char) - shift
+            if char.islower():
+                if stay < ord('a'):
+                    stay += 26
+            if char.isupper():
+                if stay < ord('A'):
+                    stay += 26
+            final = chr(stay)
+        plaintext += final
     return plaintext
